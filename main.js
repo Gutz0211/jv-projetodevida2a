8e2ea9c1c35ce983b3f3854bc93f553496ca005e
Gutz0211 <g.correa21@escola.pr.gov.br>
2026-03-31 11:21:18 -0300

    uppercase

--- main.js
+++ main.js
@@ -98,10 +98,18 @@ for (let i = 0; i < botoes.length; i++) {
 
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1); //Antes
+contadores[0].textContent = calculaTempo(tempos[0]); // Depois
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 let tempoAtual = new Date();
 
 function calculaTempo(tempoObjetivo) {
@@ -111,6 +119,7 @@ function calculaTempo(tempoObjetivo) {
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
+if (tempoFinal > 0) {
   return (
     dias +
     " dias " +
@@ -120,9 +129,37 @@ function calculaTempo(tempoObjetivo) {
     " minutos " +
     segundos +
     " segundos"
-    segundos %= 60;
-minutos %= 60;
-horas %= 24;
   );
+} else {
+  return "Prazo Finalizado";
 }
 
+// Para cada objetivo na lista de contadores
+for (let i = 0; i < contadores.length; i++) {
+  //Calcular o tempo usando a função e associá-lo ao objetivo
+  contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
+
+const diasAniversario = new Date();
+
+function calculaDias(diasAniversario) {
+    const hoje = new Date();  
+    const tempoRestante = diasAniversario - hoje; 
+    const diasRestantes = Math.floor(tempoRestante / (1000 * 60 * 60 * 24)); 
+    
+    return diasRestantes;
+}
+//comecaCronometro();
